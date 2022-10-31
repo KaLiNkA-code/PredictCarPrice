0dacd5177860ebb36c79247353aa1912d67593ef
Egor <777egorkalinin777@gmail.com>
2022-11-01 00:03:44 +0300

    Activate pre-commit-config ))

--- preprocess.py
+++ preprocess.py
@@ -4,11 +4,11 @@ from sklearn import preprocessing
 
 def text_nums_to_int(text) -> int:
     print(text)
-    if text == 'two':
+    if text == "two":
         return 2
-    elif text == 'four':
+    elif text == "four":
         return 4
-    elif text == 'six':
+    elif text == "six":
         return 6
     else:
         print(text)
@@ -17,17 +17,39 @@ def text_nums_to_int(text) -> int:
 
 def preprocess(dataset) -> pd.DataFrame:
     s_scaler = preprocessing.StandardScaler()
-    col_names = ['horsepower', 'doornumber', 'peakrpm', 'highwaympg', 
-                 'citympg', 'compressionratio', 'stroke', 'boreratio', 'enginesize', 'wheelbase', 'price']
-    dataset['doornumber_temp'] = dataset['doornumber'].apply(lambda x: text_nums_to_int(x))
+    col_names = [
+        "horsepower",
+        "doornumber",
+        "peakrpm",
+        "highwaympg",
+        "citympg",
+        "compressionratio",
+        "stroke",
+        "boreratio",
+        "enginesize",
+        "wheelbase",
+        "price",
+    ]
+    dataset["doornumber_temp"] = dataset["doornumber"].apply(lambda x: text_nums_to_int(x))
     print()
-    features = s_scaler.fit_transform([dataset['horsepower'], dataset['doornumber_temp'], dataset['peakrpm'], dataset['highwaympg'], 
-                                   dataset['citympg'], dataset['compressionratio'], dataset['stroke'], 
-                                   dataset['boreratio'], dataset['enginesize'], dataset['wheelbase']])
+    features = s_scaler.fit_transform(
+        [
+            dataset["horsepower"],
+            dataset["doornumber_temp"],
+            dataset["peakrpm"],
+            dataset["highwaympg"],
+            dataset["citympg"],
+            dataset["compressionratio"],
+            dataset["stroke"],
+            dataset["boreratio"],
+            dataset["enginesize"],
+            dataset["wheelbase"],
+        ]
+    )
     features = pd.DataFrame(features.T)
-    features.insert(loc=10, column='price', value=dataset['price'])
+    features.insert(loc=10, column="price", value=dataset["price"])
     features.columns = col_names
     return features
 
 
-print('test')
+print("test")
